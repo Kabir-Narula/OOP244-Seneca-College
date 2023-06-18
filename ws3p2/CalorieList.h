f94c68591cb81890a0f983829639ceb19ce12a2f
Levi~Ackerman <137004973+LeviAcker25@users.noreply.github.com>
2023-06-18 17:37:13 -0400

    Update CalorieList.h

--- ws3p2/CalorieList.h
+++ ws3p2/CalorieList.h
@@ -1,12 +1,4 @@
-//==============================================
-// Name           : Kabir Narula
-// Email          : Knarula9@myseneca.ca
-// Student ID     : 127962223
-// Section        : NAA
-// Date           : 06/06/2023(tuesday)
-//==============================================
-//I did all the code by myself and only copied the code given by professor 
-//provided to complete my workshops and assignments.
+
 #ifndef SDDS_CALORIELIST_H
 #define SDDS_CALORIELIST_H
 
