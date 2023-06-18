999b48eb4c995771c696d3b9fe273bfc38aa5b3a
Levi~Ackerman <137004973+LeviAcker25@users.noreply.github.com>
2023-06-18 17:45:44 -0400

    Update ShoppingRec.cpp

--- ws1/ShoppingRec.cpp
+++ ws1/ShoppingRec.cpp
@@ -1,10 +1,4 @@
-//==============================================
-// Name           : Kabir Narula
-// Email          : Knarula9@myseneca.ca
-// Student ID     : 127962223
-// Section        : NAA
-// Date           : 05/17/2023(Wednesday)
-//==============================================
+
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
