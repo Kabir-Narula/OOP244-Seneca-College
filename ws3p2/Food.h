87f0656a18b1f000bdc1e0e1d89f17b328a9b96a
Levi~Ackerman <137004973+LeviAcker25@users.noreply.github.com>
2023-06-18 17:37:33 -0400

    Update Food.h

--- ws3p2/Food.h
+++ ws3p2/Food.h
@@ -1,12 +1,4 @@
-//==============================================
-// Name           : Kabir Narula
-// Email          : Knarula9@myseneca.ca
-// Student ID     : 127962223
-// Section        : NAA
-// Date           : 06/06/2023(tuesday)
-//==============================================
-//I did all the code by myself and only copied the code given by professor 
-//provided to complete my workshops and assignments.
+
 #define _CRT_SECURE_NO_WARNINGS
 #ifndef SDDS_FOOD_H
 #define SDDS_FOOD_H
