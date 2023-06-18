ad80e97a4963f5f53d3373a919353f45211240cf
Levi~Ackerman <137004973+LeviAcker25@users.noreply.github.com>
2023-06-18 17:43:33 -0400

    Update cstring.cpp

--- ws1P2/cstring.cpp
+++ ws1P2/cstring.cpp
@@ -1,10 +1,3 @@
-//==============================================
-// Name           : Kabir Narula
-// Email          : Knarula9@myseneca.ca
-// Student ID     : 127962223
-// Section        : NAA
-// Date           : 05/17/2023(Wednesday)
-//==============================================
 
 #include <iostream>
 #include "cstring.h"
