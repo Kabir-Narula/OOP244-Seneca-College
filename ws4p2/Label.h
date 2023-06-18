ca25ed0663af4113dfa2d97d8eda65cbc7afc6bc
Levi~Ackerman <137004973+LeviAcker25@users.noreply.github.com>
2023-06-18 17:31:14 -0400

    Update Label.h

--- ws4p2/Label.h
+++ ws4p2/Label.h
@@ -1,12 +1,4 @@
-//==============================================
-// Name           : Kabir Narula
-// Email          : Knarula9@myseneca.ca
-// Student ID     : 127962223
-// Section        : NAA
-// Date           : 13/06/2023(tuesday)
-//==============================================
-//I did all the code by myself and only copied the code given by professor 
-//provided to complete my workshops and assignments.
+
 #ifndef SDDS_LABEL_H
 #define SDDS_LABEL_H
 
