3f36f34c3afeebe5be5cf6b0e32bc0247ac7eba7
Levi~Ackerman <137004973+LeviAcker25@users.noreply.github.com>
2023-06-18 17:38:56 -0400

    Update File.h

--- ws2p2/File.h
+++ ws2p2/File.h
@@ -1,12 +1,4 @@
-//==============================================
-// Name           : Kabir Narula
-// Email          : Knarula9@myseneca.ca
-// Student ID     : 127962223
-// Section        : NAA
-// Date           : 05/30/2023(tuesday)
-//==============================================
-//I did all the code by myself and only copied the code given by professor 
-//provided to complete my workshops and assignments.
+
 
 
 #ifndef FILE_H
