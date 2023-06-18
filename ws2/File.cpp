3b0268740ddc937ed8cdf89b3112dbf1a657777f
Levi~Ackerman <137004973+LeviAcker25@users.noreply.github.com>
2023-06-18 17:40:58 -0400

    Update File.cpp

--- ws2/File.cpp
+++ ws2/File.cpp
@@ -1,12 +1,3 @@
-//==============================================
-// Name           : Kabir Narula
-// Email          : Knarula9@myseneca.ca
-// Student ID     : 127962223
-// Section        : NAA
-// Date           : 05/23/2023(tuesday)
-//==============================================
-//I did all the code by myself and only copied the code given by professor 
-//provided to complete my workshops and assignments.
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <cstdio>
