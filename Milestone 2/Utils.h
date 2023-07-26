9bfba30155e18f65ab68d7a37d3599f9c2597b15
Levi~Ackerman <137004973+LeviAcker25@users.noreply.github.com>
2023-07-26 14:34:10 -0400

    Add files via upload

---
+++ Milestone 2/Utils.h
@@ -0,0 +1,33 @@
+/* Citation and Sources...
+Final Project Milestone 2
+Module: Utils.h
+Filename: Utils.h
+Version 1.0
+Author: Fardad Soleimanloo
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2023/21/07  Preliminary release
+2023/22/07  Debugged DMA
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
+
+//==============================================
+// Name           : Kabir Narula
+// Email          : Knarula9@myseneca.ca
+// Student ID     : 127962223
+// Section        : NAA
+//==============================================
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
+namespace sdds
+{
+
+
+	class Utils
+	{
+	};
+
+}
